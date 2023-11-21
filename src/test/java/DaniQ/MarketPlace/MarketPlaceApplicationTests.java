6949660c435b22cf004ee2bf9a783b934812b832
unknown <daniel.jose1999@outlook.com>
2023-11-20 19:47:57 -0500

    Primer paso Proyecto de MarketPlace

---
+++ src/test/java/DaniQ/MarketPlace/MarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package DaniQ.MarketPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
