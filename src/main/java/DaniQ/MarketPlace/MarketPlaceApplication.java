7e3b5c0b269d1978409556fb352a08287574c4f5
unknown <daniel.jose1999@outlook.com>
2023-11-21 20:55:10 -0500

    Creacion de la estructura de nuestro proyecto

--- src/main/java/DaniQ/MarketPlace/MarketPlaceApplication.java
+++ src/main/java/DaniQ/MarketPlace/MarketPlaceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarketPlaceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MarketPlaceApplication.class, args);
 	}
 
